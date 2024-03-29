a6d29b28b9b9bd0def482da92c97bd8070217f95
Prince Singh <marvelousprince012233@gmail.com>
2024-03-29 23:18:42 +0530

    push

--- app/(pages)/privacy/page.tsx
+++ app/(pages)/privacy/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Container from "@/components/Container";
 import { client } from "@sanity/lib/client";
 import { PrivacyPolicy } from "@/types/sanity";
+import { PortableText } from "@/components/PortableText";
 
 export default async function Page() {
   const data = (await client.fetch(
@@ -19,9 +20,9 @@ export default async function Page() {
             {data.header?.title}
           </p>
           <div className="mx-auto my-10 bg-slate-300 h-[1px] w-full"></div>
-          <p className="mt-8 tracking-wider text-center text-md md:text-md w-auto">
-            {data.content}
-          </p>
+          <div className="mt-8 flex items-center justify-center">
+            <PortableText value={data.content} />
+          </div>
         </div>
       </div>
     </Container>
--- app/(pages)/tnc/page.tsx
+++ app/(pages)/tnc/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Container from "@/components/Container";
 import { client } from "@sanity/lib/client";
 import { TermsAndConditions } from "@/types/sanity";
+import { PortableText } from "@/components/PortableText";
 
 const data = {
   heading: "VICTORIOUS",
@@ -26,9 +27,9 @@ export default async function Page() {
             {data.header?.title}
           </p>
           <div className="mx-auto my-10 bg-slate-300 h-[1px] w-full"></div>
-          <p className="mt-8 tracking-wider text-center text-md md:text-md w-auto">
-            {data.content}
-          </p>
+          <div className="mt-8 flex items-center justify-center">
+            <PortableText value={data.content} />
+          </div>
         </div>
       </div>
     </Container>
