d5b72005966a9206f14eace9a0d53b076ccffb9b
deepshikhalcb049 <lcb2021049@iiitl.ac.in>
2024-03-30 08:46:44 +0530

    blogIndividual

---
+++ app/components/blog/content.tsx
@@ -0,0 +1,11 @@
+import Container from "../Container";
+
+export default function Content() {
+    return (
+        <div className="h-screen flex justify-center items-center w-full">
+            <Container>
+                content
+            </Container>
+        </div>
+    )
+}
